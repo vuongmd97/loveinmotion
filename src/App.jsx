2bb9b716f185fa0b105273d7415ae93e65de57db
VuongDuy <vuongmd97@gmail.com>
2025-10-27 17:57:56 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import EnvelopeIntro from './components/EnvelopeIntro';
 import InvitationContent from './components/InvitationContent';
+import Lottie from 'lottie-react';
+import music from './assets/lottie/music.json';
 
 function App() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const lottieRef = useRef();
 
     const openEnvelope = () => {
         setIsOpen(true);
+    };
+
+    const handleToggle = () => {
+        if (isPlaying) {
+            lottieRef.current.stop();
+            setIsPlaying(false);
+        } else {
+            lottieRef.current.play();
+            setIsPlaying(true);
+        }
     };
 
     return (
@@ -14,6 +28,9 @@ function App() {
             <div className="wedding__container">
                 <EnvelopeIntro onOpen={openEnvelope} isOpen={isOpen} />
                 {isOpen && <InvitationContent />}
+                <div className="btn-play" onClick={handleToggle}>
+                    <Lottie lottieRef={lottieRef} animationData={music} loop autoplay={isOpen ? true : false} />
+                </div>
             </div>
         </div>
     );
